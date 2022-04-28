5fabaaf5b9a38a4de72e21f700a753dfff0f6f70
Eliyas Hossain <eliyashossain700@gmail.com>
2022-04-28 11:22:37 +0600

    Google Sign In Enable

--- src/Pages/Authorization/SociaSignIn/SocialSignIn.js
+++ src/Pages/Authorization/SociaSignIn/SocialSignIn.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 import "./Social.css";
 const SocialSignIn = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+  let errorMsg;
+  if (error) {
+    errorMsg = <p>{error?.message}</p>;
+  }
+  if (user) {
+    navigate("/");
+  }
   return (
     <div>
       <div className="d-flex align-items-center mx-5 px-2">
@@ -15,8 +27,9 @@ const SocialSignIn = () => {
         ></div>
       </div>
       <div className="social-icons w-75 ">
-        {/* <p className="text-danger">{errorElement}</p> */}
+        <p className="text-danger">{errorMsg}</p>
         <button
+          onClick={() => signInWithGoogle()}
           style={{ outline: "none" }}
           className="w-75  mb-3 py-2 rounded-pill border-primary"
         >
